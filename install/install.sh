829c88c4e28bfa18d93f04e5622a97625473533b
UsayHi <rafaelsamorinha@gmail.com>
2020-01-16 23:26:02 +0000

    Back Script and first idea

---
+++ install/install.sh
@@ -0,0 +1,2 @@
+pip freeze | xargs pip uninstall -y
+pip install --upgrade -r reqs.txt
---
+++ run.py
@@ -0,0 +1,10 @@
+__author__ = "Rafael Samorinha"
+__version__ = "1.0.0"
+
+
+def main():
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()
