7543450cb03098c09bbcf1a98ebec5b4409f85e9
vandam71 <rafaelsamorinha@gmail.com>
2020-05-01 23:36:06 +0100

    New Module to handle dates

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from data.reader import Reader
 from data.stock import Stock
 from datetime import datetime
 import matplotlib.pyplot as plt
-from constants import *
 
 plt.rcParams['figure.figsize'] = (10, 5)
 # TODO docstrings
@@ -15,11 +14,12 @@ def reload_data():
 
 
 def main():
-    ticker1 = Stock.from_csv("AMZN")
-    ticker1.draw_candlestick(days=50)
-    ticker1.draw_plot(days=50)
-    print(Stock.__doc__)
+    ticker1 = Stock.from_pickle("AMZN")
+    # CODE GOES HERE
+
+    # ---------------
     plt.show()
+    ticker1.dump()
 
 
 if __name__ == "__main__":
