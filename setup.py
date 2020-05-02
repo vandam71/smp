ec8cf3e32bb75cfa981dce08adf7225730d775c7
vandam71 <rafaelsamorinha@gmail.com>
2020-05-02 15:23:47 +0100

    Clean command added to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,34 @@
+from abc import ABC
+
 import package
 from setuptools import setup, find_namespace_packages
 import distutils.cmd
+import os
+import shutil
+
+ROOTDIR = os.path.dirname(os.path.abspath(__file__))
+FILES_CLEAN = ['build', 'dist', '{name}.egg-info'.format(name=package.name), '.cache']
+WALK_FILES_EXT_CLEAN = ['.pyc']
+WALK_DIRS_CLEAN = ['__pycache__']
 
 
-class PrintCommand(distutils.cmd.Command):
-    description = 'Simple Test Command'
-
-    def initialize_options(self) -> None:
-        pass
-
-    def finalize_options(self) -> None:
-        pass
+class CleanCommand(distutils.cmd.Command, ABC):
+    description = 'Project Clean'
 
     def run(self) -> None:
-        print("I am Here!")
+        for filename in FILES_CLEAN:
+            if os.path.exists(filename):
+                shutil.rmtree(filename)
+        for dirpath, dirnames, filenames in os.walk(ROOTDIR):
+            for filename in filenames:
+                for extension in WALK_FILES_EXT_CLEAN:
+                    if filename.endswith(extension):
+                        path = os.path.join(dirpath, filename)
+                        os.unlink(path)
+            for dirname in dirnames:
+                if dirname in WALK_DIRS_CLEAN:
+                    path = os.path.join(dirpath, dirname)
+                    shutil.rmtree(path, ignore_errors=True)
 
 
 def main():
@@ -30,7 +45,7 @@ def main():
         author_email=','.join([author['email'] for author in package.authors]),
         packages=find_namespace_packages(),
         cmdclass={
-            'test': PrintCommand
+            'clean': CleanCommand
                  },
         **args_setuptools
     )
